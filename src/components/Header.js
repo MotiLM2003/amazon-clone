b66e288f523ac21f5c7df787c476db8eceb3dcc5
Moti Elmakies <motiphone2003@gmail.com>
2021-09-13 21:35:38 +0300

    :apple: initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Image from 'next/image';
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+const Header = () => {
+  return (
+    <header>
+      <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2 '>
+        <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
+          <Image
+            src='https://links.papareact.com/f90'
+            width={150}
+            height={40}
+            objectFit='contain'
+            className='cursor-pointer'
+          />
+        </div>
+
+        <div className='hidden  sm:flex items-center h-10 flex-grow rounded-md bg-yellow-400 cursor-pointer hover:bg-yellow-500 '>
+          <input
+            type='text'
+            className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none  px-4 whitespace-nowrap'
+          />
+          <SearchIcon className='h-12 p-4' />
+        </div>
+
+        {/* right */}
+        <div className='text-white flex items-center text-xs space-x-6 mx-6'>
+          <div className='link'>
+            <p>Hello Moti</p>
+            <p className='font-extrabold md:text-sm'>Account & List</p>
+          </div>
+          <div className='link'>
+            <p>Return</p>
+            <p className='font-extrabold md:text-sm'>& Orders</p>
+          </div>
+          <div className='relative link flex items-center'>
+            <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
+              3
+            </span>
+            <ShoppingCartIcon className='h-10' />
+            <p className='hidden md:inline font-extrabold md:text-sm mt-2'>
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
+        <p className='link flex items-center'>
+          <MenuIcon className='h-6 mr-1' />
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Bsiness</p>
+        <p className='link'>Today's Deals</p>
+        <p className='hidden md:inline-flex'>Electronics</p>
+        <p className='hidden md:inline-flex'>Food & Grocery</p>
+        <p className='hidden md:inline-flex'>Prime</p>
+        <p className='hidden lg:inline-flex'>buy Again</p>
+        <p className='hidden lg:inline-flex'>Shopper toolkit</p>
+        <p className='hidden lg:inline-flex'>Helth & Personal Care</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Product.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Product = ({ id, title, price, description, category, image }) => {
+  return <div></div>;
+};
+
+export default Product;
